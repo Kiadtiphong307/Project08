b52ed075ca81f1b6b13e602cb647eb690ebe5085
kiadtiphong192 <kiadtiphong192@gmail.com>
2023-09-15 00:49:59 +0700

    แก้ไข

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,24 @@ import { ref,computed  } from 'vue';
 
 //variable drinking
 const travelList=ref([
-  {name: 'สุนทรารมณ์',  price:500, img:"https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/327442369_874305543617514_7552528986654738604_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeFNdHIyDFw8l9dSHVOxGbtd2Q6BSD08VdDZDoFIPTxV0Jyx8MQf2dauMZCHSslW0V1nOlqKwTeXRF6hl0wVPuBq&_nc_ohc=QGIXLCcANAEAX8oL9of&_nc_ht=scontent.fbkk13-2.fna&oh=00_AfDGtzcszEljTAHl8QKGUijSrx-jt8GwLZ5OZs6OTu4VSQ&oe=64FD9D20", quantity: 0 }, 
+  {name: 'สุนทรารมณ์',  price:500, img:"https://scontent.fbkk8-4.fna.fbcdn.net/v/t39.30808-6/327442369_874305543617514_7552528986654738604_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeFNdHIyDFw8l9dSHVOxGbtd2Q6BSD08VdDZDoFIPTxV0Jyx8MQf2dauMZCHSslW0V1nOlqKwTeXRF6hl0wVPuBq&_nc_ohc=MoJ_i7SuIaIAX-5rY5N&_nc_ht=scontent.fbkk8-4.fna&oh=00_AfDj3eShgD3NfVF8NQhSLby8MjpxmT5xzwpvXF_JyQTVfQ&oe=65078060", quantity: 0 }, 
 
-  {name:'Overnight',  price:750, img:"https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/314943429_107842845474701_641691726405226769_n.jpg?stp=cp6_dst-jpg&_nc_cat=107&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeFloVqAmAQK63gBmvRlILSFEYxzLzXtdwsRjHMvNe13C67jqz5C4ozZtnfeVB-bdVvF2m0RlI6vAA4gmgVza1hd&_nc_ohc=JsU_-083hU4AX9u2vHL&_nc_ht=scontent.fbkk13-2.fna&oh=00_AfDn433NRDpL4MAAkFVmrpWijqDVhNrFYXkfdIiOmAr2pw&oe=64FDD6B1", quantity: 0 },
+  {name:'Overnight',  price:750, img:"https://scontent.fbkk8-4.fna.fbcdn.net/v/t39.30808-6/314943429_107842845474701_641691726405226769_n.jpg?stp=cp6_dst-jpg&_nc_cat=107&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeFloVqAmAQK63gBmvRlILSFEYxzLzXtdwsRjHMvNe13C67jqz5C4ozZtnfeVB-bdVvF2m0RlI6vAA4gmgVza1hd&_nc_ohc=DyHXRvzCagcAX_EsJ8L&_nc_ht=scontent.fbkk8-4.fna&oh=00_AfDbubdImiyk7GgevdA0_oG2_FFOM9qIOlBdVlX1-OYyYA&oe=6507B9F1", quantity: 0 },
   
-  {name:'Tamma',      price:1500, img:"https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/361305041_779199773993037_7111305937619716444_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeGZ36SZ7tHE4Sn56ahHG-4VKhovrXyAa8YqGi-tfIBrxqe1tPcilruoK6M8iPurXs2EtvRAUFDpW4HyLULu-tZG&_nc_ohc=NUl9gU8b32gAX_PJpz-&_nc_ht=scontent.fbkk13-2.fna&oh=00_AfA2l1ggQGMrfsjdgRgq6o2Vr20QfaEeiIvMAwIHJ_KcXQ&oe=64FF0047", quantity: 0 },
+  {name:'Tamma',      price:1500, img:"https://scontent.fbkk8-4.fna.fbcdn.net/v/t39.30808-1/361305041_779199773993037_7111305937619716444_n.jpg?stp=dst-jpg_p320x320&_nc_cat=110&ccb=1-7&_nc_sid=754033&_nc_eui2=AeGZ36SZ7tHE4Sn56ahHG-4VKhovrXyAa8YqGi-tfIBrxqe1tPcilruoK6M8iPurXs2EtvRAUFDpW4HyLULu-tZG&_nc_ohc=VqKbsOGLFokAX8slDCK&_nc_ht=scontent.fbkk8-4.fna&oh=00_AfBEDD-Ytjt30bDHq4-O8nH6emcpf55QqEORZt6dPgRw7g&oe=65077841", quantity: 0 },
 
-  {name:'ไปยาล',     price:1599, img:"https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-1/326435697_501993212005268_2267045348590722770_n.jpg?stp=dst-jpg_p320x320&_nc_cat=103&ccb=1-7&_nc_sid=754033&_nc_eui2=AeGmFPzsMIq3BFoj1PViby3YxTbvRg4F5HPFNu9GDgXkc_ntcZ1iiswsTq9-jcH-pP3KvXD7sj7D6hgpksqlR7X8&_nc_ohc=w70TqL0LBHMAX9eRmtz&_nc_ht=scontent.fbkk13-2.fna&oh=00_AfDtcL42wBIGzSJBRdL2HOvzWz8Iouxb-CLNFaSsdyGKeQ&oe=64FDEFC9", quantity: 0 },
+  {name:'ไปยาล',     price:1599, img:"https://scontent.fbkk8-4.fna.fbcdn.net/v/t39.30808-6/326435697_501993212005268_2267045348590722770_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeGmFPzsMIq3BFoj1PViby3YxTbvRg4F5HPFNu9GDgXkc_ntcZ1iiswsTq9-jcH-pP3KvXD7sj7D6hgpksqlR7X8&_nc_ohc=lq-ZZCnaPS4AX98BUoc&_nc_ht=scontent.fbkk8-4.fna&oh=00_AfBJ14fKjCxrqISqNLziSVrQdwHfq1wfmCLxouSEENt66w&oe=6508A1CF", quantity: 0 },
 
-  {name:'Too nice',    price:1699, img:"https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/292496235_1007767096737663_225747972753273688_n.png?_nc_cat=110&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeElcXBRemzlCc00lIr4Gg4HzTWRn5lngH3NNZGfmWeAfdM0r7nLsyIZaexMz8_UB8jL6tg2M4vxxRmT3iKLZbmm&_nc_ohc=R0AqwtXy2XQAX-6usnI&_nc_ht=scontent.fbkk13-2.fna&oh=00_AfCKCH-GD6spsF2crGqpoQLClRFzW_2zxeLiQdyhl16SXQ&oe=64FDB576", quantity: 0 },
-
-
-  {name:'ท่าช้าง',  price:1890,  img:"https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/350942472_940270750565444_6986476706008406745_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeGOw7uk2VFH7K0Y1VV60YyZJnOKgtUtkswmc4qC1S2SzHt3w5MKrvNCf6Jeuix0rENdMiRxxWccKJITy2tbgpFm&_nc_ohc=EBl4NSGR99UAX9T9gPf&_nc_ht=scontent.fbkk13-2.fna&oh=00_AfCxWFHC52mx2q6QlRYlCfWtamuHpgvz2Rl7wsWrb3JeQw&oe=64FE7920", quantity: 0 },
+  {name:'Too nice',    price:1699, img:"https://scontent.fbkk8-4.fna.fbcdn.net/v/t39.30808-1/292496235_1007767096737663_225747972753273688_n.png?stp=dst-png_p320x320&_nc_cat=110&ccb=1-7&_nc_sid=754033&_nc_eui2=AeElcXBRemzlCc00lIr4Gg4HzTWRn5lngH3NNZGfmWeAfdM0r7nLsyIZaexMz8_UB8jL6tg2M4vxxRmT3iKLZbmm&_nc_ohc=2hWztmABB_sAX_Q6Nb3&_nc_ht=scontent.fbkk8-4.fna&oh=00_AfAHyTW93_EDeJFNeNvKt1aCY4J7V8Et238UqX7FEJ4gMA&oe=65082E34", quantity: 0 },
 
 
-  {name:'Beer lab',      price:675, img:"https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/326776461_983306725977390_1286925122631726978_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeF1Ywb4j0jzAKiBj2OSSgPDDbJxNGoeGtoNsnE0ah4a2rGXYUI_AKkz9jMg_OiM6XP4-mQuh4pgkXOJfXGs7H2T&_nc_ohc=C2NpKwZz5ZcAX-iRwGx&_nc_ht=scontent.fbkk13-2.fna&oh=00_AfCGwZMuYRbBc1i9Ivz1WNNLf6IBiCJgzMhZd5ZieomqYQ&oe=64FE3E79", quantity: 0 },
+  {name:'ท่าช้าง',  price:1890,  img:"https://scontent.fbkk8-4.fna.fbcdn.net/v/t39.30808-6/375029827_709973494492365_1738695314107857060_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeFL9-N37V6xpl9ybQQ7FBZA_6qgl7AQYeb_qqCXsBBh5lt1bpfvTLtD65N7cmCm6wJ7aQxkIQ18ZJlw_4iPCI5y&_nc_ohc=hZLyC7KhOMUAX-flrgt&_nc_ht=scontent.fbkk8-4.fna&oh=00_AfDLQCJzMkHVY6sNeIyd7HD9fC03km1W7krt4Sjm-KceQw&oe=65080193", quantity: 0 },
 
 
-  {name:'Warm up',     price:1250, img:"https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/326148970_1521569305000988_2338094468162899732_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a2f6c7&_nc_aid=0&_nc_eui2=AeHdrRzQLjAXk3z9zYSatohZJSB-z1el8TIlIH7PV6XxMsYI3VPYXaWft8sKQRkEMdnn378aOgRBez-gwkIXATgA&_nc_ohc=-nloMFue0PQAX-oPt_-&_nc_ht=scontent.fbkk13-2.fna&oh=00_AfBXrWHaV-03hPUW252v3dkcfrL-3dEN5uWBqRGG3hOVzw&oe=64FF1E18", quantity: 0 },
+  {name:'Beer lab',      price:675, img:"https://scontent.fbkk8-4.fna.fbcdn.net/v/t39.30808-6/326776461_983306725977390_1286925122631726978_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeF1Ywb4j0jzAKiBj2OSSgPDDbJxNGoeGtoNsnE0ah4a2rGXYUI_AKkz9jMg_OiM6XP4-mQuh4pgkXOJfXGs7H2T&_nc_ohc=co9_cYeTCnQAX8h_sKB&_nc_ht=scontent.fbkk8-4.fna&oh=00_AfBXog7J8RRIOiUyVxMYbqKsin8Dy-whbfaEhNpElNeiDw&oe=650821B9", quantity: 0 },
+
+
+  {name:'Warm up',     price:1250, img:"https://scontent.fbkk8-4.fna.fbcdn.net/v/t39.30808-6/326148970_1521569305000988_2338094468162899732_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a2f6c7&_nc_aid=0&_nc_eui2=AeHdrRzQLjAXk3z9zYSatohZJSB-z1el8TIlIH7PV6XxMsYI3VPYXaWft8sKQRkEMdnn378aOgRBez-gwkIXATgA&_nc_ohc=66rSvEUftNUAX8qlmPU&_nc_ht=scontent.fbkk8-4.fna&oh=00_AfAcBF_nKR-NR_O1qNp7tJtBiCGCZQ7ggBix_l0oR7szfQ&oe=65090158", quantity: 0 },
 ])
 
 
@@ -83,9 +83,6 @@ const totalCost = computed(() => {
     return list_local.value.reduce((acc, item) => acc + item.price, 0);
 });
 
-function displayPopup() {
-    showPopup.value = true;
-}
 
 function togglePopup() {
   showPopup.value = !showPopup.value;
@@ -166,7 +163,7 @@ function togglePopup() {
   
   <div class="contrainer_costumer " v-if="list_local.length>0">
   <div class="form_box"> <h2 style="text-align: center;">กรุณากรอกข้อมูล</h2></div>
-  <form>
+  <form >
     <div class="form_box">
       <label for="name">ชื่อ-นามสกุล </label><br>
       <input type="text" id="name" class="form-control" v-model="form_costumer.name" required>
@@ -193,6 +190,7 @@ function togglePopup() {
 <div v-if="showPopup" class="overlay">
 
   <div class="popup">
+      <div class="card-icon"> <img src="https://cdn-icons-png.flaticon.com/512/5610/5610944.png" alt="" srcset=""> </div>
       <h2 style="text-align: center; color:green ;">ยืนยันการจองสำเร็จ</h2>
       <h3 >รายละเอียด</h3>
       <p><strong>ชื่อ-นามสกุล:</strong> {{ form_costumer.name }}</p>
@@ -288,6 +286,12 @@ function togglePopup() {
 
 .header{
   padding: 25px;
+}
+
+.card-icon img{
+  height: 125px;
+  padding: 10px;
+  margin-left: 430px;
 }
 
 </style>
